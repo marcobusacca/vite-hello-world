7170d5855c1103fb9273c2a069f71ccc881474cc
marcobusacca <marcobusacca01@gmail.com>
2023-06-19 16:50:04 +0200

    Esercizio Completato

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <!-- SCRIPT JS -->
 <script>
+  // IMPORTO LA COMPONENTE APP TITLE
+  import AppTitle from './components/AppTitle.vue';
+
   export default {
-    
+    // COMPONENTI
+    components:{
+      // DICHIARO LA COMPONENTE APP TITLE
+      AppTitle,
+    }
   }
 </script>
 
 <!-- TEMPLATE HTML -->
 <template lang="">
   <div>
+    <!-- USO LA COMPONENTE APP TITLE IN HTML -->
+    <AppTitle/>
   </div>
 </template>
 
