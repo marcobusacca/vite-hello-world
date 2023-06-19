e375ddcca65fed31cd9c70f9ac56958768cbc467
marcobusacca <marcobusacca01@gmail.com>
2023-06-19 16:44:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- SCRIPT JS -->
+<script>
+  export default {
+    
+  }
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+  <div>
+  </div>
+</template>
+
+<!-- STYLE CSS -->
+<style lang="">
+  
+</style>
